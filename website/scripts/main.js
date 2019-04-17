2ce6e4ee85d5009aed34658b3b343d91c19ba453
MalinSpaniol <mspaniol@uos.de>
2019-04-17 19:37:34 +0200

    new Version with new background an dnew feature with codeword

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -1,22 +1,10 @@
 
-
-
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-
-function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
+function greet()
+{
+  var codeword = document.getElementById('name').value;
+  if (codeword == "mellon" || codeword == "Mellon") {
+alert('Welcome, ' + document.getElementById('name').value + '. Please enter our mines!');
+}else{
+  alert("you shall not pass")
 }
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
-}
-
-myButton.onclick = function() {
-  setUserName();
 }
